b9db5b493d94e01d5e690060833966037197e502
Cristopher Ferrari <ferraricristopher93@gmail.com>
2023-12-21 16:13:27 +0100

    main Js: rimozione import Appheader: Costruzione struttura

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,9 +5,47 @@ export default {
 </script>
 
 <template>
-    <h1>Header</h1>
+    <header>
+        <div class="container">
+            <img src="../assets/img/dc-logo.png" alt="">
+            <ul>
+                <li>
+                    <a class="active" href="">
+                        Prova
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </header>
 </template>
 
 <style lang="scss" scoped>
 // SCSS ONLY COMPONENT
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
+
+.container {
+    display: flex;
+    justify-content: space-between;
+
+    ul {
+        list-style: none;
+        @include center();
+
+
+        li a {
+            display: inline-block;
+            padding: 2.5rem 1rem;
+            text-decoration: none;
+            font-weight: 600;
+
+
+            &.active,
+            &:hover {
+                border-bottom: 3px solid #0282f9;
+                color: #0282f9;
+            }
+        }
+    }
+}
 </style>
